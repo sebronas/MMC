ee7e9fd47bab1608dbb5fee3dd34da20468dc6c9
Kristaps Sebris <sebriskristaps@gmail.com>
2022-07-08 14:39:20 +0300

    [Created] - Created project skeleton

---
+++ src/main/java/com/example/mmc/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.mmc;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.mmc {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.mmc to javafx.fxml;
+    exports com.example.mmc;
+}
