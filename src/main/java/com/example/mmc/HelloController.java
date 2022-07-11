0e7b40d29113155beb49089184714866f43ce508
Elena Bebrish <bebrish.elena@gmail.com>
2022-07-11 18:48:50 +0300

    controller.java

--- src/main/java/com/example/mmc/HelloController.java
+++ src/main/java/com/example/mmc/HelloController.java
@@ -1,14 +1,45 @@
 package com.example.mmc;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
-    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+   /* @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }*/
+
+    public void switchToHelloView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public void switchToLIST(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("LIST.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 }
