9956b551958858d4325242e113cec0afa034c5bc
Georgijs Kadoļčiks <georgs.k@apollo.lv>
2022-07-12 10:52:58 +0300

    Further cleanup

--- src/main/java/com/example/mmc/HelloApplication.java
+++ src/main/java/com/example/mmc/HelloApplication.java
@@ -6,17 +6,29 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class HelloApplication extends Application {
+
+    Connection connection;
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LIST.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 780, 540);
-        stage.setTitle("List!");
+        stage.setTitle("List");
         stage.setScene(scene);
         stage.show();
+//        try {
+//            connection = DriverManager.getConnection("jdbc:...");
+//        }
+//        catch (SQLException sqlException) {
+//            sqlException.printStackTrace();
+//            System.exit(1);
+//        }
     }
+
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/com/example/mmc/HelloController.java
+++ src/main/java/com/example/mmc/HelloController.java
@@ -1,23 +1,23 @@
 package com.example.mmc;
 
-import javafx.event.ActionEvent;
-
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
 public class HelloController {
 
-    Connection connection;
+    @FXML
+    ListView studentList;
 
-    public void initialize() {
-//        try {
-//            connection = DriverManager.getConnection("jdbc:...");
-//        }
-//        catch (SQLException sqlException) {
-//            sqlException.printStackTrace();
-//            System.exit(1);
-//        }
-    }
+    @FXML
+    ListView gradeList;
+
+    @FXML
+    ListView instrumentList;
+
+    @FXML
+    ListView teacherList;
+
+    @FXML
+    ListView accompanistList;
+
 }
