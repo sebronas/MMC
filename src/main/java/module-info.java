327b6671d94187486dedf8ea909c640a8e3d81e3
sebronas <sebriskristaps@gmail.com>
2022-07-19 09:11:50 +0300

    [changes] - removed lombok

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,7 +10,6 @@ module com.example.mmc {
     requires java.persistence;
     requires mysql.connector.java;
     requires java.sql;
-    requires lombok;
 
     opens com.example.mmc to javafx.fxml;
     opens com.example.mmc.Model to org.hibernate.orm.core;
