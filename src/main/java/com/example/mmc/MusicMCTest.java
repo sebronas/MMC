b7b98c7ed5077f40619915cba009b08b57f98e11
Elena Bebrish <bebrish.elena@gmail.com>
2022-07-19 18:53:59 +0300

    JUnit tests?

---
+++ src/main/java/com/example/mmc/MusicMCTest.java
@@ -0,0 +1,16 @@
+package com.example.mmc;
+
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MusicMCTest {
+
+    @Test
+    void start() {
+    }
+
+    @org.junit.Test
+    void main() {
+    }
+}
