bc22074776c366d9bd54f524e1a57e0138d063b5
Georgijs Kadoļčiks <georgs.k@apollo.lv>
2022-07-20 14:41:29 +0300

    Refactor using DAO

--- src/main/java/com/example/mmc/MusicMCController.java
+++ src/main/java/com/example/mmc/MusicMCController.java
@@ -1,7 +1,6 @@
 package com.example.mmc;
 
-import com.example.mmc.Model.StudentEntity;
-import com.example.mmc.Utility.HibernateUtil;
+import com.example.mmc.DAO.StudentDAO;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -14,16 +13,8 @@ import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import org.hibernate.Session;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
-import java.sql.DriverManager;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 /** "Music Master's Companion" JavaFX Controller class
  *  @author Kristaps Sebris, Elena Bebrisa, Georgijs Kadolciks
@@ -53,50 +44,26 @@ public class MusicMCController {
     @FXML
     public ListView accompanistList;
 
-    // Declares and instantiates TreeSet fields for automatic sorting and filtering distinct data
-    private final TreeSet<String> studentSet = new TreeSet<>();
-    private final TreeSet<String> gradeSet = new TreeSet<>();
-    private final TreeSet<String> instrumentSet = new TreeSet<>();
-    private final TreeSet<String> teacherSet = new TreeSet<>();
-    private final TreeSet<String> accompanistSet = new TreeSet<>();
-
-    // Declares and instantiates ObservableList fields for storing ListView data
+    // Declares and instantiates ObservableList fields for storing list view data
     private final ObservableList<String> studentObservList = FXCollections.observableArrayList();
     private final ObservableList<String> gradeObservList = FXCollections.observableArrayList();
     private final ObservableList<String> instrumentObservList = FXCollections.observableArrayList();
     private final ObservableList<String> teacherObservList = FXCollections.observableArrayList();
     private final ObservableList<String> accompanistObservList = FXCollections.observableArrayList();
 
-    // Declares and instantiates ObservableList fields for storing user's selected ListView data
-    private final ObservableList<String> selectedStudentList = FXCollections.observableArrayList();
-    private final ObservableList<String> selectedGradeList = FXCollections.observableArrayList();
-    private final ObservableList<String> selectedInstrumentList = FXCollections.observableArrayList();
-    private final ObservableList<String> selectedTeacherList = FXCollections.observableArrayList();
-    private final ObservableList<String> selectedAccompanistList = FXCollections.observableArrayList();
+    // Instantiates a StudentDAO for data access
+    StudentDAO studentDAO = new StudentDAO();
 
-    // Declares a List for storing all data
-    private List<StudentEntity> allStudents;
-
-    /** Initializes Controller
-     */
     public void initialize() {
 
-        // Opens a Hibernate session, loads all data and closes the session
-        Session session = HibernateUtil.getSession();
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery query = builder.createQuery(StudentEntity.class);
-        Root<StudentEntity> root = query.from(StudentEntity.class);
-        allStudents = session.createQuery(query).getResultList();
-        session.close();
-
-        // Associates ListViews with ObservableLists
+        // Associates list views with observable lists
         studentList.setItems(studentObservList);
         gradeList.setItems(gradeObservList);
         instrumentList.setItems(instrumentObservList);
         teacherList.setItems(teacherObservList);
         accompanistList.setItems(accompanistObservList);
 
-        // Sets multiple selection mode for the ListViews
+        // Sets multiple selection mode for the list views
         studentList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         gradeList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         instrumentList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
@@ -118,59 +85,29 @@ public class MusicMCController {
     @FXML
     private void onRequestOrReloadAction(ActionEvent event) {
 
-        // Loads user's selections into ObservableList fields
-        selectedStudentList.setAll(studentList.getSelectionModel().getSelectedItems());
-        selectedGradeList.setAll(gradeList.getSelectionModel().getSelectedItems());
-        selectedInstrumentList.setAll(instrumentList.getSelectionModel().getSelectedItems());
-        selectedTeacherList.setAll(teacherList.getSelectionModel().getSelectedItems());
-        selectedAccompanistList.setAll(accompanistList.getSelectionModel().getSelectedItems());
+        // Filters data on user's selection
+        studentDAO.filterData(
+                studentList.getSelectionModel().getSelectedItems(),
+                gradeList.getSelectionModel().getSelectedItems(),
+                instrumentList.getSelectionModel().getSelectedItems(),
+                teacherList.getSelectionModel().getSelectedItems(),
+                accompanistList.getSelectionModel().getSelectedItems());
 
-        // Uses streams for filtering data based on user's selections, and stores results in another list
-        List<StudentEntity> filteredStudents = allStudents.stream()
-                .filter(st -> selectedStudentList.isEmpty()
-                        || selectedStudentList.contains(st.getStudent()))
-                .filter(st -> selectedGradeList.isEmpty()
-                        || selectedGradeList.contains(st.getGrade()))
-                .filter(st -> selectedInstrumentList.isEmpty()
-                        || selectedInstrumentList.contains(st.getInstrumentId().getInstrument()))
-                .filter(st -> selectedTeacherList.isEmpty()
-                        || selectedTeacherList.contains(st.getTeacherId().getTeacher()))
-                .filter(st -> selectedAccompanistList.isEmpty()
-                        || selectedAccompanistList.contains(st.getAccompanistId().getAccompanist()))
-                .collect(Collectors.toList());
-
-        // Resets TreeSets
-        studentSet.clear();
-        gradeSet.clear();
-        instrumentSet.clear();
-        teacherSet.clear();
-        accompanistSet.clear();
-
-        // Makes filtered data ordered and distinct by adding them to TreeSets
-        for (StudentEntity st : filteredStudents) {
-            studentSet.add(st.getStudent());
-            gradeSet.add(st.getGrade());
-            instrumentSet.add(st.getInstrumentId().getInstrument());
-            teacherSet.add(st.getTeacherId().getTeacher());
-            accompanistSet.add(st.getAccompanistId().getAccompanist());
-        }
-
-        // Populates ObservableLists with filtered, ordered, and distinct data from the TreeSets
-        studentObservList.setAll(studentSet);
-        gradeObservList.setAll(gradeSet);
-        instrumentObservList.setAll(instrumentSet);
-        teacherObservList.setAll(teacherSet);
-        accompanistObservList.setAll(accompanistSet);
+        // Populates observable lists with filtered, ordered, and distinct data
+        studentObservList.setAll(studentDAO.getStudents());
+        gradeObservList.setAll(studentDAO.getGrades());
+        instrumentObservList.setAll(studentDAO.getInstruments());
+        teacherObservList.setAll(studentDAO.getTeachers());
+        accompanistObservList.setAll(studentDAO.getAccompanists());
     }
 
-    /** Adds students' list or user's selections to the editable text area
+    /** Adds students' list or user's selection to the editable text area
      */
     @FXML
     private void onListAddAction(ActionEvent event) {
-        ObservableList<String> selectedList = FXCollections.observableArrayList();
-        selectedList.setAll(studentList.getSelectionModel().getSelectedItems());
-        if (!selectedList.isEmpty()) {
-            for (String string : selectedList)
+        List<String> selectedStudents = new ArrayList<>(studentList.getSelectionModel().getSelectedItems());
+        if (!selectedStudents.isEmpty()) {
+            for (String string : selectedStudents)
                 textArea.appendText(string + "\n");
             return;
         }
